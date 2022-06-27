8acd5c927cf039b32603f86c1279a0f89f365c8d
Pawan Shrestha <pawanshresthait@gmail.com>
2022-06-27 12:50:38 +0545

    setup .env

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ const connectionString =
   'mongodb+srv://pawanstha:1234@pmscollection-ecommerce.4uekm.mongodb.net/taskmanager?retryWrites=true&w=majority';
 
 const connectDB = url => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
